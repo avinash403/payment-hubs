f1029e503cace1d0c3194f4b1e7761c6c20b78b2
avinash kumar <alerts@ladybirdweb.com>
2020-10-29 20:13:00 +0530

    Made home page as dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,18 @@ use App\Http\Controllers\StripePaymentController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('dashboard'));
+    //return view('welcome');
 });
 
+Route::get('dashboard', [PaymentGatewayController::class, 'index'])->name('dashboard');
+
+Route::post('payment-gateway', [PaymentGatewayController::class, 'store'])->name('payment-gateway');
 
 // passing either mysql Id in the url and based on that querying
 Route::get('stripe/{appId}', [StripePaymentController::class, 'stripe'])->name('payment.stripe.view');
 Route::post('stripe/{appId}/payment-process', [StripePaymentController::class, 'process'])->name('payment.stripe.process');
 
-Route::get('dashboard', [PaymentGatewayController::class, 'index']);
-Route::post('payment-gateway', [PaymentGatewayController::class, 'store'])->name('payment-gateway');
 
 Route::get('paypal/{appId}', [\App\Http\Controllers\PaypalPaymentController::class, 'paypal'])->name('payment.paypal.view');
 Route::post('paypal/{appId}/payment-process', [\App\Http\Controllers\PaypalPaymentController::class, 'payWithPaypal'])->name('payment.paypal.process');
